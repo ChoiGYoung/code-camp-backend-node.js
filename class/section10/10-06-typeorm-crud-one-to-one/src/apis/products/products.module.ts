d2ead09c304a757b4d01420c8513d65462478284
ChoiGYoung <choidh1261@naver.com>
2024-05-27 18:26:44 +0900

    1:1 관계 등록 API

---
+++ class/section10/10-06-typeorm-crud-one-to-one/src/apis/products/products.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { ProductsResolver } from './product.resolver';
+import { ProductsService } from './product.service';
+import { ProductsSaleslocationsService } from '../productsSaleslocations/productsSaleslocations.service';
+import { ProductSaleslocation } from '../productsSaleslocations/entities/productSaleslocation.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Product, //
+      ProductSaleslocation,
+    ]),
+  ],
+  providers: [
+    ProductsResolver, //
+    ProductsService, //
+    ProductsSaleslocationsService,
+  ],
+})
+export class ProductsModule {}
