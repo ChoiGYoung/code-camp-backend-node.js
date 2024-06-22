6ed1401d4c4fd50838b3fc0e23b7ca408063b4f9
ChoiGYoung <choidh1261@naver.com>
2024-06-22 17:41:52 +0900

    feat) files.service.ts 버킷 추가

--- class/section17/17-01-deploy-with-docker/backend/src/apis/files/files.service.ts
+++ class/section17/17-01-deploy-with-docker/backend/src/apis/files/files.service.ts
@@ -4,28 +4,35 @@ import { Storage } from '@google-cloud/storage';
 
 @Injectable()
 export class FilesService {
-  upload({ file }: IFilesServiceUpload): string {
-    console.log(file);
+  async upload({ files }: IFilesServiceUpload): Promise<string[]> {
+    console.log(files);
+
+    const waitedFiles = await Promise.all(files);
+    console.log(waitedFiles); // [File, File]
 
     // 1. 파일을 클라우드 스토리지에 저장하는 로직
 
     // 1-1) 스토리지 셋팅하기
+    const bucket = 'storage-file-codecamp';
     const storage = new Storage({
-      projectId: '',
-      keyFilename: '',
-    }).bucket('');
+      projectId: 'backend-426115',
+      keyFilename: 'backend-426115-05fca5319d31.json',
+    }).bucket(bucket);
 
     // 1-2) 스토리지에 파일 올리기
-    file
-      .createReadStream()
-      .pipe(storage.file(file.filename).createWriteStream())
-      .on('finish', () => {
-        console.log('성공');
-      })
-      .on('error', () => console.log('실패'));
+    const results = await Promise.all(
+      waitedFiles.map(
+        (el) =>
+          new Promise<string>((resolve, reject) => {
+            el.createReadStream()
+              .pipe(storage.file(el.filename).createWriteStream())
+              .on('finish', () => resolve(`${bucket}/${el.filename}`))
+              .on('error', () => reject('실패'));
+          }),
+      ),
+    );
 
-    console.log('파일전송이 완료되었습니다.');
-
-    return '끝';
+    // 2. 다운로드URL 브라우저에 돌려주기
+    return results;
   }
 }
